add254fd400a02eddcdd7e774e65aa1787984669
thiagonf <thiagonf10@gmail.com>
2019-02-28 11:43:55 -0300

    Adding methods index and check login

--- project/loginApp/views.py
+++ project/loginApp/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
 
-# Create your views here.
+import json
+import logging
+import requests
+
+from django.contrib.auth.mixins import (
+    LoginRequiredMixin,
+    PermissionRequiredMixin
+)
+from django.views.generic.base import TemplateView
+from jose.exceptions import JWTError
+
+import django_keycloak.services.oidc_profile
+import django_keycloak.services.remote_client
+
+
+logger = logging.getLogger(__name__)
+
+
+class Home(TemplateView):
+    template_name = 'home.html'
+
+class Permission(PermissionRequiredMixin, TemplateView):
+    raise_exception = True
+    template_name = 'permission.html'
+    permission_required = 'some-permission'
