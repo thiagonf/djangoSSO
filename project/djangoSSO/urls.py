852f55dc35f9468f189b3b2d8f65bbd1115e69fd
thiagonf <thiagonf10@gmail.com>
2019-03-04 13:05:41 -0300

    Fixing url method

--- project/djangoSSO/urls.py
+++ project/djangoSSO/urls.py
@@ -17,7 +17,10 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls import url
 
+from loginApp import views
+
 urlpatterns = [
+    url(r'^$', views.Index.as_view(), name='index'),
     path('admin/', admin.site.urls),
     url(r'^keycloak/', include('django_keycloak.urls')),
     url(r'^login/', include('loginApp.urls')),
