10a1996138cf9b7bc1fbfb61d8b8acb4639c524c
thiagonf <thiagonf10@gmail.com>
2019-03-07 14:07:31 -0300

    Adding in index pages roles are that logging user

--- project/loginApp/views.py
+++ project/loginApp/views.py
@@ -20,40 +20,55 @@ import django_keycloak.services.remote_client
 logger = logging.getLogger(__name__)
 
 class Index(LoginRequiredMixin, TemplateView):
-    template_name = 'home.html'
+    template_name = 'index.html'
     login_url = '/login'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        jwt = get_decoded_jwt(self)
+        
+        if jwt is not None:
+            roles = jwt['realm_access']['roles']
+        else:
+            roles = ""
+        
+        context['roles'] = roles
+
+        return context
+
 
 class Login(TemplateView):
     template_name = 'home.html'
 
     def get_context_data(self, **kwargs):
+        print("entrou")
         context = super().get_context_data(**kwargs)
-        jwt = self.get_decoded_jwt()
+        jwt = get_decoded_jwt(self)
+        print(jwt)
         
         if jwt is not None:
             jwt=json.dumps(jwt, sort_keys=True, indent=4, separators=(',', ': '))
         else:
             jwt = ""
-
+        
         context['jwt'] = jwt
+
         return context  
-    
-    def get_decoded_jwt(self):
-        if not hasattr(self.request.user, 'oidc_profile'):
-            return None
-
-        oidc_profile = self.request.user.oidc_profile
-        client = oidc_profile.realm.client
-
-        return client.openid_api_client.decode_token(
-            token=oidc_profile.access_token,
-            key=client.realm.certs,
-            algorithms=client.openid_api_client.well_known[
-                'id_token_signing_alg_values_supported']
-        )
 
 
 class Permission(LoginRequiredMixin, TemplateView):
-    raise_exception = True
     template_name = 'permission.html'
-    permission_required = 'some-permission'
+
+def get_decoded_jwt(self):
+    if not hasattr(self.request.user, 'oidc_profile'):
+        return None
+
+    oidc_profile = self.request.user.oidc_profile
+    client = oidc_profile.realm.client
+
+    return client.openid_api_client.decode_token(
+        token=oidc_profile.access_token,
+        key=client.realm.certs,
+        algorithms=client.openid_api_client.well_known[
+            'id_token_signing_alg_values_supported']
+    )
